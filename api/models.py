9a0e1a60e59e95943af12009abf4d03190a1f894
DeWeW <dewel000per@gmail.com>
2025-07-16 12:09:45 +0500

    first API

--- api/models.py
+++ api/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-class Category(models.Model):
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+class Category(BaseModel):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs) -> None:
         self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+
+class Post(BaseModel):
+    pass
