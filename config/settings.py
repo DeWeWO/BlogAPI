9262ba9e35b1214e1d7d9f922234931d2864ffe7
DeWeW <dewel000per@gmail.com>
2025-07-17 17:15:18 +0500

    first API

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,6 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
