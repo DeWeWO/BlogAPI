487a6b9bb15777bd62c5ff7558c236e100ddf97f
DeWeW <dewel000per@gmail.com>
2025-07-17 16:51:45 +0500

    first API

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
 
 if settings.DEBUG:
