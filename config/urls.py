dd471fc6713a6fb214a4233048aa8c2f923309c3
DeWeW <dewel000per@gmail.com>
2025-07-18 13:17:33 +0500

    first API

--- config/urls.py
+++ config/urls.py
@@ -17,3 +17,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Refresh token
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc1MjkxMTk3NiwiaWF0IjoxNzUyODI1NTc2LCJqdGkiOiJiMTkzMGYyYzcxZGM0N2I1OTFjYWFiNmFmMWM4YTcyMiIsInVzZXJfaWQiOjJ9.fSVm8lZtopBkEah5jNZ3X7hH3eoYd5WBBMTuebcvGjU
