1c9b9a66359bb6f0d78336eae09dee7f0f563b21
DeWeW <dewel000per@gmail.com>
2025-07-18 18:04:35 +0500

    first API

--- api/auth/sign_up.py
+++ api/auth/sign_up.py
@@ -1,13 +1,14 @@
 from api.serializers import RegisterSerializer
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 
 class RegisterAPIView(GenericAPIView):
+    permission_classes = (permissions.AllowAny, )
     serializer_class = RegisterSerializer
     
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         token = serializer.save()
-        return Response(data={"token": token.key}, status=status.HTTP_201_CREATED)
+        return Response({}, status=status.HTTP_201_CREATED)
