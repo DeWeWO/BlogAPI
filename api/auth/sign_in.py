0c304dd4f2c98584a8013fc80225b800ac701557
DeWeW <dewel000per@gmail.com>
2025-07-21 18:05:44 +0500

    first API

--- api/auth/sign_in.py
+++ api/auth/sign_in.py
@@ -11,5 +11,3 @@ class LoginAPIView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         token = serializer.save()
         return Response(data={"token": token.key}, status=status.HTTP_201_CREATED)
-
-
